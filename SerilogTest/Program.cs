3bea7f71fb9b5b5377ddbd99e6b1f7c39c2114ee
AJ <ales.jaterka@fullsys.cz>
2019-11-11 15:38:49 +0100

    Vytvoření testovacího programu s nastaveními

---
+++ SerilogTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace SerilogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vytvoření konfigurace z JSON souboru (může být i zanořená ve webapi.json, serilog si vezme svoji část)
+            var configuration = new ConfigurationBuilder().AddJsonFile("serilog_cfg.json").Build();
+
+            // using zde používám jen pro nanucení dispose aby se odeslal ten email hnedka
+            using (var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger())
+            {
+                // Otestování jednotlivých záznamů, podle nastavení v JSON se budou zapisovat 
+                // pouze do konzole
+                logger.Verbose("Zapisuji: Verbose");
+                logger.Debug("Zapisuji: Debug");
+                
+                // do konzole a do souboru
+                logger.Information("Zapisuji: Information");
+                logger.Warning("Zapisuji: Warning");
+                logger.Error("Zapisuji: Error");
+
+                // do konzole, souboru, a odešle se i email
+                logger.Fatal("Zapisuji: Fatal");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
